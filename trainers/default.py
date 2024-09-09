d6c7062cf24cbd114e932b40b5bc7393c139b0d4
Jane Du <zd53@cornell.edu>
2024-09-08 19:20:32 -0500

    add soem forced zeros

--- trainers/default.py
+++ trainers/default.py
@@ -74,6 +74,7 @@ def train(
     max_score_meter = AverageMeter("max_score", ":6.4f")
     l1_meter = AverageMeter("l1", ":6.4f")
     zero_count_meter = AverageMeter("zero_count", ":6.4f")
+
     l = [
         loss_meter,
         train_nll_meter,
@@ -385,12 +386,12 @@ def modifier(args, epoch, model):
 def proj_sort(model, z, rho_tolerance):
     v = model.fc.weight.data.flatten()
     dim_v = v.shape[0]
-    if torch.norm(v, 1) <= z:
-        return
-    
+    # if torch.norm(v, 1) <= z:
+    #     return
+
     signs = torch.sign(v)
     mu, p = torch.sort(v.abs(), descending=True)
-    # signs[p] 
+    # signs[p]
     rho = dim_v - 1
     for i in range(dim_v):
         res = mu[i] - (torch.sum(mu[:i]) - z) / (i + 1)
@@ -398,10 +399,12 @@ def proj_sort(model, z, rho_tolerance):
             rho = i - 1
             break
 
+    rho = min(rho, dim_v - rho_tolerance)
+    # even if l1 norm is satisfied, kill some weights
+
     # theta = mu[dim_v - rho_tolerance :].mean()
     theta = (torch.sum(mu[:rho]) - z) / (rho + 1)
     trimmed = (mu - theta).clamp(min=0)
-
 
     model.fc.weight.data = (trimmed[p] * signs).reshape(model.fc.weight.shape)
     print("num zeros", (model.fc.weight == 0).sum().item())
