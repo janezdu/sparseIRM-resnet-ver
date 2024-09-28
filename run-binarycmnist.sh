d47480d549a60631ddaa79cc61da9c8d9aafe0b3
Jane Du <zd53@cornell.edu>
2024-09-28 02:46:17 -0500

    chose 25

--- run-binarycmnist.sh
+++ run-binarycmnist.sh
@@ -1,7 +1,7 @@
 # # dense/irmv1 run
-# for rhotval in 40
+for rhotval in 25
 # for rhotval in 20 40 60 80 100
-for rhotval in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 135 155 175 195
+# for rhotval in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 135 155 175 195
 do
 
 # # # gumbel
